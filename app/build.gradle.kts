d544bcf414b2ee219399c8b92777812d849cdf12
stevezmtstudios <98326195+SteveZMTstudios@users.noreply.github.com>
2026-03-25 17:47:04 +0800

    Add new launcher icons
    - Added various resolutions of the launcher icon in WEBP format:
      - mipmap-hdpi
      - mipmap-mdpi
      - mipmap-xhdpi
      - mipmap-xxhdpi
      - mipmap-xxxhdpi
    - Created a new XML resource file for the launcher background color with hex value #3576A4.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "top.stevezmt.wearos.bluetoothadb.daemon"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
